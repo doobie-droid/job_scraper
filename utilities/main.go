9cec1a6322475a75e3aed264f7596a3ec2d7c46e
doobie-droid <lesliedouglas23@gmail.com>
2025-02-23 17:14:08 +0100

    feat: add new method to use default keys in the instance that they do not exist in the .env files

--- utilities/main.go
+++ utilities/main.go
@@ -1,9 +1,11 @@
 package utilities
 
 import (
-	"github.com/joho/godotenv"
+	"fmt"
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func GetEnv(key string) string {
@@ -11,5 +13,16 @@ func GetEnv(key string) string {
 	if err != nil {
 		log.Fatal("Error loading .env file", err.Error())
 	}
+	if os.Getenv(key) == "" {
+		panic(fmt.Sprintf("pls load .env key %s in the .env file", key))
+	}
 	return os.Getenv(key)
+}
+
+func GetEnvOrUseDefault(key string, defaultValue string) string {
+	envValue := GetEnv(key)
+	if envValue == "" {
+		return defaultValue
+	}
+	return envValue
 }
