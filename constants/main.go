aba44704f1c806a01c9457217b87e2ef28bc9963
ObsidianBlaze <wijojames@gmail.com>
2025-02-28 02:11:45 +0100

    feat: created a helper method to manage retrieving information form the .env file

--- constants/main.go
+++ constants/main.go
@@ -1,7 +1,7 @@
 package constants
 
 import (
-	"doobie-droid/job-scraper/utilities"
+	"os"
 	"strings"
 	"time"
 )
@@ -12,16 +12,16 @@ const DB_TIMEOUT = 3 * time.Second
 
 // TODO change GetENV to config usage
 
-var LINKEDIN_EMAIL = utilities.GetEnv("LINKEDIN_EMAIL")
-var LINKEDIN_PASSWORD = utilities.GetEnv("LINKEDIN_PASSWORD")
-var VALID_KEYWORDS = utilities.GetEnv("VALID_KEYWORDS")
-var RAPID_API_KEY = utilities.GetEnv("RAPID_API_KEY")
-var RAPID_API_URL = utilities.GetEnv("RAPID_API_URL")
-var DATE_POSTED string = utilities.GetEnvOrUseDefault("DATE_POSTED", "past24Hours")
-var JOB_KEYWORD string = utilities.GetEnvOrUseDefault("JOB_KEYWORD", "golang")
-var LOCATION string = utilities.GetEnvOrUseDefault("LOCATION", "NGA")
-var LOCATION_TYPE string = utilities.GetEnvOrUseDefault("LOCATION_TYPE", "Remote")
-var CITY string = utilities.GetEnvOrUseDefault("CITY", "LAGOS")
+var LINKEDIN_EMAIL = getEnv("LINKEDIN_EMAIL", "")
+var LINKEDIN_PASSWORD = getEnv("LINKEDIN_PASSWORD", "")
+var VALID_KEYWORDS = getEnv("VALID_KEYWORDS", "")
+var RAPID_API_KEY = getEnv("RAPID_API_KEY", "")
+var RAPID_API_URL = getEnv("RAPID_API_URL", "")
+var DATE_POSTED string = getEnv("DATE_POSTED", "past24Hours")
+var JOB_KEYWORD string = getEnv("JOB_KEYWORD", "golang")
+var LOCATION string = getEnv("LOCATION", "NGA")
+var LOCATION_TYPE string = getEnv("LOCATION_TYPE", "Remote")
+var CITY string = getEnv("CITY", "LAGOS")
 
 func GetLinkedInDurationCode() string {
 	DurationOfPostingToDurationCode := map[string]string{"past24hours": "r86400", "pastweek": "r604800", "pastmonth": "r2592000"}
@@ -44,4 +44,11 @@ func GetLinkedInLocationType() string {
 	LocationTypeToIds := map[string]string{"remote": "2", "on-site": "1", "hybrid": "3"}
 	return LocationTypeToIds[strings.ToLower(LOCATION_TYPE)]
 
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
 }
