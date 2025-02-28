2f2995754dfd6142937719673ec6c497d3ad9246
ObsidianBlaze <wijojames@gmail.com>
2025-02-28 21:25:38 +0100

    test: checks if the right location id is returned for a country and returns and empty string for unmapped locations(countries).

--- constants/main_test.go
+++ constants/main_test.go
@@ -17,7 +17,25 @@ func TestGetLinkedInLocationType(t *testing.T) {
 		LOCATION_TYPE = tc.input
 		got := GetLinkedInLocationType()
 		if got != tc.expected {
-			t.Errorf("GetLinkedInLocationType(%s) = %s; want %s", tc.input, got, tc.expected)
+			t.Errorf("GetLinkedInLocationType(%s) expected = %s; but got %s", tc.input, got, tc.expected)
+		}
+	}
+}
+
+func TestGetLinkedInLocationId(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{"NGA", "105365761"},
+		{"GHA", ""},
+	}
+
+	for _, tc := range tests {
+		LOCATION = tc.input
+		got := GetLinkedInLocationId()
+		if got != tc.expected {
+			t.Errorf("GetLinkedInLocationId(%s) expected = %s; but got %s", tc.input, got, tc.expected)
 		}
 	}
 }
