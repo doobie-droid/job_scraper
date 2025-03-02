7b00587126b222b37260f72907cba350c4bccc4c
doobie-droid <lesliedouglas23@gmail.com>
2025-03-02 17:16:53 +0100

    test: add test to check that isValidLocation can be correctly identified

--- data/job_test.go
+++ data/job_test.go
@@ -1,6 +1,8 @@
 package data
 
 import (
+	"doobie-droid/job-scraper/config"
+	"fmt"
 	"testing"
 )
 
@@ -24,4 +26,30 @@ func TestJobIsValid(t *testing.T) {
 		}
 	}
 
+}
+
+func TestLocationIsValid(t *testing.T) {
+	testCases := []struct {
+		locationString  string
+		ExpectedOutcome bool
+	}{
+		{fmt.Sprintf(GetBaseLocationString(), "EU"), false},
+		{fmt.Sprintf(GetBaseLocationString(), "Worldwide"), true},
+		{fmt.Sprintf(GetBaseLocationString(), "Nigeria"), true},
+		{fmt.Sprintf(GetBaseLocationString(), config.NewConfig().City), true},
+		{fmt.Sprintf(GetBaseLocationString(), config.NewConfig().Location), true},
+	}
+
+	for _, testCase := range testCases {
+		job := new(Job)
+		locationIsValid := job.IsValidLocation(testCase.locationString)
+		if locationIsValid != testCase.ExpectedOutcome {
+			t.Errorf("Expected location is valid to be %t, Got %t", testCase.ExpectedOutcome, locationIsValid)
+		}
+	}
+
+}
+
+func GetBaseLocationString() string {
+	return `Ardent Blue Technologies\n\n Linkedin profile Location: 🇪🇺 %s Remote, 100% Remote \n\n\n Skills wanted, in addition to Golang:devops\ngo\ngolang\nphp\n`
 }
