a5b41e04661884bac8b98c484673d06e4aab8e10
ObsidianBlaze <wijojames@gmail.com>
2025-03-02 01:53:24 +0100

    test: test to get workable duration code

--- constants/main_test.go
+++ constants/main_test.go
@@ -39,3 +39,24 @@ func TestGetLinkedInLocationId(t *testing.T) {
 		}
 	}
 }
+
+func TestGetWorkableDurationCode(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{"past24Hours", "1"},
+		{"pastWeek", "7"},
+		{"pastMonth", "30"},
+		{"invalid", ""},
+	}
+
+	for _, tc := range tests {
+		DATE_POSTED = tc.input
+		got := GetWorkableDurationCode()
+		if got != tc.expected {
+			t.Errorf("GetWorkableDurationCode(%s) expected = %s; but got %s", tc.input, got, tc.expected)
+
+		}
+	}
+}
