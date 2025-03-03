608da17456b05cca1047fbcec7289ce4b6bd02bd
doobie-droid <lesliedouglas23@gmail.com>
2025-03-03 11:23:07 +0100

    fix: alter db to ensure that it uses utf8 encoding for a richer character set

--- sql/job_migration.sql
+++ sql/job_migration.sql
@@ -4,6 +4,9 @@
 -- Dumped from database version 5.7 (Ubuntu 22.)
 USE job_scraper;
 
+-- Ensure database uses utf8mb4 so that complex characters and emojis from job titles are valid
+ALTER DATABASE job_scraper CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- TODO
 -- ADD DOCKER TO PROJECT FOR EASY SETUP AND UPDATE README FILE WHEN YOU ADD DOCKER
 CREATE TABLE jobs (
